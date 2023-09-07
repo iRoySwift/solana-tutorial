ead7f798e63287527e6320d7f446785408167571
Roy <iRoySwift@iCloud.com>
2023-09-07 20:13:03 +0800

    invoke hello world

--- solana-program-part2/invoke-helloworld/cli/src/invoke_cli.rs
+++ solana-program-part2/invoke-helloworld/cli/src/invoke_cli.rs
@@ -1,15 +1,14 @@
 use std::str::FromStr;
 
-use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     instruction::{AccountMeta, Instruction},
     pubkey::Pubkey,
-    system_instruction,
 };
 use solana_rpc_client::rpc_client::RpcClient;
 use solana_sdk::{signature::Keypair, signer::Signer, transaction::Transaction};
 
-const PROGRAM_ID: &str = "9eMNGtayMEuNkzfdUYSw8k9msaPhFJG9Bi75wGQDvddR";
+const HELLOWORLD_PROGRAM_ID: &str = "9eMNGtayMEuNkzfdUYSw8k9msaPhFJG9Bi75wGQDvddR";
+const INVOKE_PROGRAM_ID: &str = "D4saMj1EGsMCkRVmf46iHSmpTH7GzACvBdZF4WLvbE1r";
 const DEV_NET: &str = "https://qn-devnet.solana.fm";
 const PRIVATE_KEY: [u8; 64] = [
     37, 37, 60, 131, 98, 125, 34, 130, 135, 2, 57, 248, 169, 60, 174, 216, 219, 70, 59, 155, 64, 7,
@@ -18,51 +17,31 @@ const PRIVATE_KEY: [u8; 64] = [
     94,
 ];
 
-#[derive(BorshSerialize, BorshDeserialize)]
-struct GreetingAccount {
-    counter: u32,
-}
-
 pub fn invoke_program() {
-    // data
-    let greeting_account = GreetingAccount { counter: 0 };
-    let mut buffer: Vec<u8> = Vec::new();
-    greeting_account.serialize(&mut buffer).unwrap();
-    let greeting_size = buffer.len();
-
     // Step 1 连接到Solana网络 devnet
     let client = RpcClient::new(DEV_NET);
 
     // Step 2 创建者账号信息（private key）
-    let hello_program_id = Pubkey::from_str(PROGRAM_ID).unwrap();
+    let hello_program_id = Pubkey::from_str(HELLOWORLD_PROGRAM_ID).unwrap();
+    let invoke_program_id = Pubkey::from_str(INVOKE_PROGRAM_ID).unwrap();
     let signer = Keypair::from_bytes(&PRIVATE_KEY).unwrap();
-    let greeting_account_kp = Keypair::new();
 
-    // Step 3 instruction
-    let lamports = client
-        .get_minimum_balance_for_rent_exemption(greeting_size)
-        .unwrap();
-    let create_greeting_ix = system_instruction::create_account(
-        &signer.pubkey(),
-        &greeting_account_kp.pubkey(),
-        lamports,
-        greeting_size as u64,
-        &hello_program_id,
+    let instraction = Instruction::new_with_bytes(
+        invoke_program_id,
+        "hello".as_bytes(),
+        vec![
+            AccountMeta::new(signer.pubkey(), true),
+            AccountMeta::new_readonly(hello_program_id, false),
+        ],
     );
-
-    let instraction = Instruction::new_with_borsh(
-        hello_program_id,
-        &greeting_account,
-        vec![AccountMeta::new(greeting_account_kp.pubkey(), true)],
-    );
-    let ixs = vec![create_greeting_ix, instraction];
+    let ixs = vec![instraction];
 
     // Latest hash
     let recent_blockhash = client.get_latest_blockhash().unwrap();
     let tx = Transaction::new_signed_with_payer(
         &ixs,
         Some(&signer.pubkey()),
-        &[&signer, &greeting_account_kp],
+        &[&signer],
         recent_blockhash,
     );
 
--- solana-program-part2/invoke-helloworld/program/src/lib.rs
+++ solana-program-part2/invoke-helloworld/program/src/lib.rs
@@ -1,14 +1,37 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    instruction::{AccountMeta, Instruction},
+    msg,
+    program::invoke,
+    pubkey::Pubkey,
+};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// Declare and export the program's entrypoint
+entrypoint!(process_instruction);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+// Program entrypoint's implementation
+pub fn process_instruction(
+    _program_id: &Pubkey, // Public key of the account the hello world program was loaded into
+    accounts: &[AccountInfo], // The account to say hello to
+    _instruction_data: &[u8], // Ignored, all helloworld instructions are hellos
+) -> ProgramResult {
+    // Iterating accounts is safer than indexing
+    let accounts_iter = &mut accounts.iter();
+
+    // Get the account to say hello to
+    let account = next_account_info(accounts_iter)?;
+    let helloworld_program_id = next_account_info(accounts_iter)?;
+
+    msg!("invoke helloworld program entrypoint from {}", account.key);
+
+    let instruction = Instruction::new_with_bytes(
+        *helloworld_program_id.key,
+        "hello".as_bytes(),
+        vec![AccountMeta::new(*account.key, true)],
+    );
+
+    let account_infos = [account.clone()];
+    invoke(&instruction, &account_infos)
 }
